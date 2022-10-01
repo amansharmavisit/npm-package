5ffa66e293bdaddfa8fa6b2ca1e84aa51e524d34
Aman Sharma <aman.sharma@getvisitapp.com>
2022-10-01 14:34:32 +0530

    Added View and function

---
+++ src/index.js
@@ -0,0 +1,20 @@
+export default function Test ({
+    encrypted,
+    clientId,
+}) {
+    return (
+        <div>
+            <p>
+                Hello this the view you get when you call. 
+                Data - {encrypted}
+                ClientId - {clientId}
+            </p>
+        </div>
+    );
+}
+
+
+export function testFunction (...params) {
+    console.log(params);
+    return params;
+}
